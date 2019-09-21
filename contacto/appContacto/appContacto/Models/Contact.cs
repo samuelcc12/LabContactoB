8a0cd46ee7d11edf380a98d446feb638d44fbdcf
samuel.cabrera.1999@gmail.com <samuel.cabrera.1999@gmail.com>
2019-09-20 21:17:31 -0400

    Con Api service

---
+++ contacto/appContacto/appContacto/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace appContacto.Models
+{
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook
+    }
+
+    public class Contact
+    {
+        public int ContactID { get; set; }
+        public string Name { get; set; }
+        public TypeContact Type { get; set; }
+        public string ContactValue { get; set; }
+    }
+}
